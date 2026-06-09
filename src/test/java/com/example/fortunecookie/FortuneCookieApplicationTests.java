6abaf93d3be75310cb0136f80f04795f42912f88
minky5004 <minky5004@gmail.com>
2026-06-09 17:00:40 +0900

    chore: Spring Boot 프로젝트 초기 설정

---
+++ src/test/java/com/example/fortunecookie/FortuneCookieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fortunecookie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FortuneCookieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
